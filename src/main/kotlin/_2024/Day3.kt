196767a2be8d5f3fba6f85e863cd977937355335
Patryk Stradomski <patrykstradomski@wp.pl>
2024-12-04 16:51:34 +0100

    2024 Day 3 formatting

--- src/main/kotlin/_2024/Day3.kt
+++ src/main/kotlin/_2024/Day3.kt
@@ -1,11 +1,10 @@
 package com.bubber.aoc._2024
 
 import com.bubber.aoc.Puzzle
-import kotlin.math.abs
 
 class Day3(input: String) : Puzzle(input) {
     override fun solvePartOne(): Int {
-        val input = inputLines.reduce{ acc, s -> acc + s}
+        val input = inputLines.reduce { acc, s -> acc + s }
         val regex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
         val nums = Regex("\\d{1,3}")
         var sum = 0
@@ -16,16 +15,16 @@ class Day3(input: String) : Puzzle(input) {
     }
 
     override fun solvePartTwo(): Int {
-        val input = inputLines.reduce{ acc, s -> acc + s}
+        val input = inputLines.reduce { acc, s -> acc + s }
         val regex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)|don't\\(\\)|do\\(\\)")
         val nums = Regex("\\d{1,3}")
         var sum = 0
         var flag = true
         val instructions = regex.findAll(input).map { it.value }
         instructions.forEach {
-            if (it == "don't()"){
+            if (it == "don't()") {
                 flag = false
-            } else if (it == "do()"){
+            } else if (it == "do()") {
                 flag = true
             } else {
                 if (flag) {
