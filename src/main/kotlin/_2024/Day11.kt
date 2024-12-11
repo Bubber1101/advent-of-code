019565eac22a61566b03f714008c26df4451e938
Patryk Stradomski <patrykstradomski@wp.pl>
2024-12-11 17:15:33 +0100

    2024 day 11 part 2

--- src/main/kotlin/_2024/Day11.kt
+++ src/main/kotlin/_2024/Day11.kt
@@ -4,40 +4,44 @@ import com.bubber.aoc.Puzzle
 
 class Day11(input: String) : Puzzle(input) {
 
-    override fun solvePartOne(): Int {
-        var sum = 0
-        var stones = inputLines[0].split(" ").map(String::toLong)
-        val temp = mutableListOf<Int>()
-
-        for(i in 1..25){
-            stones = blink(stones)
-            println(":" + i + " " + stones.size)
-        }
-
-
-        return stones.size
-    }
-
-    fun blink(stones: List<Long>): List<Long> {
-        var temp = mutableListOf<Long>()
-        for (stone in stones) {
-            when {
-                stone == 0L -> temp.add(1)
-                stone.toString().length and 1 == 0 -> {
-                    val string = stone.toString()
-                    temp.add(string.subSequence(0,string.length/2).toString().toLong())
-                    temp.add(string.subSequence(string.length/2, string.length).toString().toLong())
-                }
-                else -> {temp.add(stone*2024)}
-            }
-        }
-        return temp
-
-    }
-
-    override fun solvePartTwo(): Int {
-        var sum = 0
+    var cheat = mutableMapOf<Pair<Int,Long>, Long>()
+    override fun solvePartOne(): Long {
+        cheat = mutableMapOf()
+        var sum = 0L
+        for (stone in inputLines[0].split(" ").map(String::toLong)) {
+            sum++
+            sum+= blink(stone, 0, 25)}
         return sum
     }
 
+    fun blink(stone: Long, count: Int, max: Int): Long {
+        var sum = 0L
+        if (count==max) return sum
+        if(cheat.containsKey(count to stone)){
+            return cheat[count to stone]!!
+        }
+        when {
+            stone == 0L -> sum += blink(1,count + 1, max)
+            stone.toString().length and 1 == 0 -> {
+                val string = stone.toString()
+                sum++
+                sum += blink(string.subSequence(0,string.length/2).toString().toLong(), count + 1, max)
+                sum += blink(string.subSequence(string.length/2, string.length).toString().toLong(), count + 1, max)
+            }
+            else -> {sum += blink(stone*2024, count + 1, max)}
+
+        }
+            cheat[count to stone] = sum
+
+        return sum
+    }
+
+    override fun solvePartTwo(): Long {
+        cheat = mutableMapOf()
+        var sum = 0L
+        for (stone in inputLines[0].split(" ").map(String::toLong)) {
+                sum++
+                sum+= blink(stone, 0, 75)}
+        return sum
+    }
 }
